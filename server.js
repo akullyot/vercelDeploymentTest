cc818aefd6eeae9bde0214cedd724b618ffc2de3
akullyot <akullyot@gmail.com>
2023-09-25 19:17:22 -0500

    added a vercel deployment json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Initialize Express and include env variables
 require('dotenv').config();
+const PORT = process.env.PORT;
 const express = require('express');
 
 const app     = express();
@@ -13,4 +14,8 @@ app.get('/', (request,response) =>
     response.send('Hello, World!')
 });
 
-app.listen(3000);
+app.listen(PORT, () => {console.log(`Server is running in port ${PORT}`)});
+
+
+//For the purposes of vercel, turn this into a serverless function by exporting my express function into vercels build proces
+module.exports = app;
